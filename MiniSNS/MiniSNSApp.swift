fef013e57f2dcda8d1a02698ae8cc62168c89462
nanashiki <nanashiki.app@gmail.com>
2021-08-23 15:49:07 +0900

    Initial Commit

---
+++ MiniSNS/MiniSNSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniSNSApp.swift
+//  MiniSNS
+//
+//  Created by nanashiki on 2021/08/23.
+//
+
+import SwiftUI
+
+@main
+struct MiniSNSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
